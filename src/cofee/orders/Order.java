bd250370ae7d190f46a20189fc7802fcdf9d46c2
dmitrikll <dgalytskyi@gmail.com>
2024-04-07 22:45:04 +0300

    add task

---
+++ src/cofee/orders/Order.java
@@ -0,0 +1,20 @@
+package cofee.orders;
+
+public class Order {
+    private static int orderCounter = 0;
+    private int orderNumber;
+    private String customerName;
+
+    public Order(String customerName) {
+        this.orderNumber = orderCounter += 1;
+        this.customerName = customerName;
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+}
